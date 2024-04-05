9d8c5856d3c7a05802f87e8ffdd350f05f987ded
Andrey <andreikgd2000@gmail.com>
2024-04-05 22:00:04 +0200

    update

--- C++/laba1/func.cpp
+++ C++/laba1/func.cpp
@@ -8,9 +8,9 @@ namespace al {
         std::ofstream file(filename, std::ios_base::app);
 
         if (file.is_open()) {
-            auto now = std::chrono::system_clock::now();
-            std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-            file << std::ctime(&now_c);
+            auto now = std::chrono::system_clock::now(); //получаем текущее время
+            std::time_t now_c = std::chrono::system_clock::to_time_t(now); //преобразуем время в тип std::time_t(кол-во секунд с начала эпохи)
+            file << std::ctime(&now_c); //вывод времени в файл
             file.close();
         }
     }
