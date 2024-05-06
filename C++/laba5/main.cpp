ff85cb285c0247d6b1aaa66a52050b47152b5b5c
Andrey <andreikgd2000@gmail.com>
2024-05-06 22:40:47 +0200

    add laba5

---
+++ C++/laba5/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <SFML/Graphics.hpp>
+#include <Circle.h>
+#include <Game.h>
+
+
+int main()
+{
+    la::Game game(1500, 900, "Circles bounce game!!!!!!!");
+    game.Setup(10);
+
+    game.LifeCycle();
+
+    return 0;
+}
